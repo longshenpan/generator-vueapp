87474df7de4a89ff612f35314854dd4665145624
zp <zhangpan8812@163.com>
2018-04-20 12:58:46 +0800

    对脚手架，添加基于express和webpack的动态构建功能

---
+++ generators/app/templates/build/webpack.base.conf.js
@@ -0,0 +1,44 @@
+/**
+ * webpack 配置文件
+ */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// 清除文件的插件
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+// 抽取css文件成单独的文件
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+module.exports = {
+	mode: 'development',
+	entry: {
+		index: ['webpack-hot-middleware/client?reload=true', './src/main.js']
+	},
+	output: {
+		filename: '[name].[hash:8].js',
+		path: path.resolve(__dirname, 'dist')// 此处必须是绝对地址，否则会报错
+	},
+	devtool: 'inline-source-map',//使用source-map
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({
+			  	fallback: "style-loader",
+			  	use: "css-loader"
+			}))
+			// css-loader是处理import @import引入的css文件和url()引入的文件等
+			// style-loader是将css文件通过style插入到html中	
+			// use: ['style-loader', 'css-loader']
+		}]
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist']),
+		new ExtractTextPlugin({
+			filename: './[name].[hash:8].css'
+		}),
+		new HtmlWebpackPlugin({template: './index.html'}),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.DefinePlugin({
+			HOMEAPI: JSON.stringify('homeapi')
+		})
+	]
+}
